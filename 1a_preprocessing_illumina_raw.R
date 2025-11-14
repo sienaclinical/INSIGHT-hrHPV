e34f2a08c089e2218d34624a0fa5959e03d6a791
sienaclinical <siena.sains.medika@gmail.com>
2025-11-14 15:24:52 +0700

    Update 1a_preprocessing_illumina_raw.R

--- 1a_preprocessing_illumina_raw.R
+++ 1a_preprocessing_illumina_raw.R
@@ -1,3 +1,9 @@
+## IMPORTANT NOTE ##
+# Preprocessing Pipeline for Illumina Methylation Array Data
+# Purpose: This script demonstrates preprocessing steps for raw IDAT files from Illumina platforms
+# Platforms: Illumina HumanMethylation450 (450K) and Illumina MethylationEPIC (850K)
+# Dataset: GSE99511 (Illumina HumanMethylation450)
+
 # --------------------- PART 1 ------------------------------------ #
 # Preprocess raw datasets downloaded from GEO repository
 # You need: the ZIP files (raw) ready on your local repository
@@ -9,7 +15,6 @@ if (!requireNamespace("BiocManager", quietly = TRUE))
 BiocManager::install(c("GEOquery", "IlluminaHumanMethylation450kmanifest",
                        "IlluminaHumanMethylation450kanno.ilmn12.hg19", 
                        "minfi", "DMRcate"))
-
 # DO: set working directory, where the downloaded data is stored
 # For example: setwd("C:/Users/Documents/GSE99511")
 
@@ -19,12 +24,10 @@ BiocManager::install(c("GEOquery", "IlluminaHumanMethylation450kmanifest",
 library(GEOquery)
 # DO: download manually the files from the GEO repository
 # Unzip IDAT files 
-untar("GSE99511/GSE99511_RAW.tar", 
-      exdir = "C:/Users/putri/Documents/GSE99511/IDATs")
+untar("GSE99511/GSE99511_RAW.tar", exdir = "GSE99511/IDATs")
 
 # -------- making sample_name file -------- #
-file_list <- list.files("C:/Users/Documents/GSE99511/IDATs", 
-                        full.names = TRUE)
+file_list <- list.files("GSE99511/IDATs", full.names = TRUE)
 filenames <- file_list[-c(1:6)]
 
 sample_sheet = data.frame(matrix(NA, nrow = length(filenames), 
@@ -54,12 +57,14 @@ gset <- gset[[idx]]
 sample_diseaseStatus <- data.frame(disease = gset@phenoData[[1]], 
                                    sampleID = gset@phenoData[[2]])
 
-samplesheet = merge(Sample_Sheet, sample_diseaseStatus, by = "sampleID", all = TRUE)
+samplesheet = merge(Sample_Sheet, sample_diseaseStatus, 
+                    by = "sampleID", all = TRUE)
 colnames(samplesheet) = c("Sample_Name", "Sentrix_ID", "Sentrix_Position",
                           "Sample_Group")
 write.csv(samplesheet, file="SampleSheet.csv", row.names = FALSE, 
           quote=FALSE)
 # save this csv on the IDATs folder
+# you only need to this once
 
 # ########################################################### #
 # Read IDAT files and create RGChannelSet                     #
@@ -67,7 +72,7 @@ write.csv(samplesheet, file="SampleSheet.csv", row.names = FALSE,
 library(minfi)
 
 # Locate IDAT files
-idat_dir <- "C:/Users/Documents/Selected studies/Final selection/GSE99511"
+idat_dir <- "GSE99511"
 targets <- read.metharray.sheet(idat_dir)
 
 # Read raw IDAT files
@@ -78,6 +83,7 @@ rgSet <- read.metharray.exp(targets = targets)
 # ########################################################### #
 library(minfi)
 library(IlluminaHumanMethylation450kanno.ilmn12.hg19) #depends on the platform used
+library(IlluminaHumanMethylationEPICanno.ilm10b4.hg19)
 library(tidyr)
 library(dplyr)
 
@@ -90,20 +96,39 @@ Beta <- getBeta(mSet)
 # ########################################################### #
 # Mapping probes into genes level                             #
 # ########################################################### #
-# Get the annotation data
-anno <- getAnnotation(IlluminaHumanMethylation450kanno.ilmn12.hg19)
+# Get the annotation data.
+anno <- getAnnotation(IlluminaHumanMethylation450kanno.ilmn12.hg19) 
+# Platform-Specific Configuration
+# NOTE: Adjust the platform parameter based on your dataset's Illumina technology
+# For Illumina MethylationEPIC: use "IlluminaHumanMethylationEPICanno.ilm10b4.hg19"
 
 # Match CpG IDs in your beta matrix to annotation
 common_cpgs <- intersect(rownames(Beta), rownames(anno))
 beta_matrix <- Beta[common_cpgs, ]
 anno <- anno[common_cpgs, ]
 
-# Create a mapping of CpGs to genes
+# remove missing Regulatory_Feature_Group + unclassified
+promoter_probes <- anno[anno$Regulatory_Feature_Group %in% c("Promoter_Associated", 
+                                                             "Promoter_Associated_Cell_type_specific",
+                                                             "Gene_Associated",
+                                                             "Gene_Associated_Cell_type_specific"), ]
+
+# Filter for CpG islands only
+cpg_island_probes <- promoter_probes[promoter_probes$Relation_to_Island == "Island", ]
+
+# Match CpG IDs in your beta matrix to annotation
+common_cpgs <- intersect(rownames(Beta), rownames(cpg_island_probes))
+beta_matrix <- Beta[common_cpgs, ]
+anno <- anno[common_cpgs, ]
+
+# 6. Extract gene symbols
 cpg_gene_map <- strsplit(as.character(anno$UCSC_RefGene_Name), ";")
 
+# 7. Create mapping dataframe
 cpg_to_gene <- data.frame(
   CpG = rep(rownames(beta_matrix), sapply(cpg_gene_map, length)),
-  Gene = unlist(cpg_gene_map)
+  Gene = unlist(cpg_gene_map),
+  stringsAsFactors = FALSE
 )
 
 # Merge beta values and gene mappings
@@ -123,4 +148,4 @@ offset = 0.000001
 Mvals0 <- log2((gene_level_beta[,-1]+offset) / (1-gene_level_beta[,-1]+offset))
 rownames(Mvals0) = as.character(gene_level_beta$Gene)
 
-save(gene_level_beta, Mvals0, group, file="dat_genes.RData")
+save(gene_level_beta, Mvals0, group, file="methdat_GSE99511.RData")

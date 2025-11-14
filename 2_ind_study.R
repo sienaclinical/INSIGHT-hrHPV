e126e7147ef66fd49ffb899ca21a7c14c0d527c7
sienaclinical <siena.sains.medika@gmail.com>
2025-11-14 15:17:45 +0700

    Update 2_ind_study.R

--- 2_ind_study.R
+++ 2_ind_study.R
@@ -11,8 +11,7 @@
 
 library(limma)
 
-load("dat_genes.RData")
-load("C:/Users/Documents/Selected studies/Final selection/list_genes.RData")
+load("dat_genes.RData") ; load("list_genes.RData")
 
 ## take the common probes only
 im = match(list_genes$common_genes, rownames(Mvals0))
@@ -38,23 +37,23 @@ n_cont = length(which(group=="control"))
 n_case = length(which(group=="case"))
 N = n_cont + n_case
 
-#hedges correction
+# hedges correction
 J = 1 - (3/(4*N - 9)) 
 
-#observe modeling result from limma
+# observe modeling result from limma
 res_table = topTable(fit, number=nrow(Mvals))
 
 # EFFECT SIZE 
 teta_ij = xbar_cont_ij = xbar_case_ij = var_cont_ij = var_case_ij = matrix()
 for(i in 1:nrow(Mvals)) {
 
-  # ---- we need this for latter, to calculate BETWEEN STUDY VARIANCE
+  # ---- we need this for latter, to calculate BETWEEN STUDY VARIANCE ------------------ #
   xbar_cont_ij[i] = mean(as.numeric(Mvals[i,which(group=="control")]), na.rm=TRUE) 
   xbar_case_ij[i] = mean(as.numeric(Mvals[i,which(group=="case")]), na.rm=TRUE)
   
   var_cont_ij[i] = var(as.numeric(Mvals[i,which(group=="control")]), na.rm=TRUE) 
   var_case_ij[i] = var(as.numeric(Mvals[i,which(group=="case")]), na.rm=TRUE) 
-  # -------
+  # ------------------------------------------------------------------------------------ #
   
   xbar = xbar_case_ij[i] - xbar_cont_ij[i]
   teta_ij[i] = (xbar/s2_est[i]) * J
@@ -65,6 +64,6 @@ summary(teta_ij)
 result_GSE99511 = data.frame(teta_ij,s2_est, xbar_cont_ij, xbar_case_ij,
                              var_cont_ij, var_case_ij)
 rownames(result_GSE9951) = rownames(Mvals)
-head(result_GSE99511)
 
 save(result_GSE99511, file="result_MA_GSE99511.RData")
+# run this script for all datasets, and save the output by naming it with the corresponding GEO-ID 
